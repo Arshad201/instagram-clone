4b221874c025990f0b75f8d2bfc4b42006f7328b
shafiya <kmushafiya003@gmail.com>
2022-12-31 12:02:24 +0000

    done with suggestion section

--- components/sidebar/sidebar.php
+++ components/sidebar/sidebar.php
@@ -1,3 +1,83 @@
-<aside>
+<div class="side-section">
     
-</aside>
+        <div class="acc">
+            <img src="image/dp1.jpg" alt="">
+            <div class="names">
+                <p class="handle">livesss_on_saturn</p>
+                <p class="name">Cara lavigne</p>
+            </div>
+            
+            <div class="switch"> <a href="#">Switch</a></div>
+    </div>
+
+  <div class="acc">
+    <p class="name">Suggestions for you</p>
+    <a href="#" class="see">See All</a>
+  </div>
+
+  <div class="acc bcc">
+            <img src="image/s1.jpg" alt="">
+            <div class="names">
+                <p class="handle">kz393849</p>
+                <p class="name">New to Instagram</p>
+            </div>
+            <div class="zero"> <a href="#">Switch</a></div>
+           
+    </div>
+
+    <div class="acc bcc">
+            <img src="image/s2.jpg" alt="">
+            <div class="names">
+                <p class="handle">_bulletproof_</p>
+                <p class="name">Followed by alfiya.07</p>
+            </div>
+            <div class="one"> <a href="#">Switch</a></div>
+          
+    </div>
+
+    <div class="acc bcc">
+            <img src="image/s3.jpg" alt="">
+            <div class="names">
+                <p class="handle">mistake_manager</p>
+                <p class="name">Follows you</p>
+            </div>
+            <div class="two"> <a href="#">Switch</a></div>
+    </div>
+
+    <div class="acc bcc">
+            <img src="image/s4.jpg" alt="">
+            <div class="names">
+                <p class="handle">miss_latte</p>
+                <p class="name">Followed by h.kh_03</p>
+            </div>
+            <div class="three"> <a href="#">Switch</a></div>
+    </div>
+
+    <div class="acc bcc">
+            <img src="image/s5.jpg" alt="">
+            <div class="names">
+                <p class="handle">cats_world</p>
+                <p class="name">New to Instagram</p>
+            </div>
+            <div class="four"> <a href="#">Switch</a></div>
+    </div>
+
+    
+    
+
+    <div class="extra">
+        <ul class="links">
+            <li><a href="#">About</a></li>
+            <li><a href="#">.Help</a></li>
+            <li><a href="#">.Press</a></li>
+            <li><a href="#">.API</a></li>
+            <li><a href="#">.Jobs</a></li>
+            <li><a href="#">.Privacy</a></li>
+            <li><a href="#">.Terms</a></li>
+            <li><a href="#">.Location<br></a></li>
+            
+            <li><a href="#">.Language</a></li>
+        </ul>
+        <div class="copyright">&copy; 2022 INSTAGRAM FROM META</div>
+    </div>
+</div>

187ea35abfec8eb93240869c50d909f96e5b3e8e
Mushafiya <kmushafiya003@gmail.com>
2023-01-04 12:28:26 +0000

    Post-section Completed

--- components/post/post.php
+++ components/post/post.php
@@ -1,5 +1,6 @@
 <div class="container">
     <div class="cards">
+        <div class="upper-part">
         <div class="name">
             <div class="part1">
               <img class ="dp-2" src="image/perfect.jpg" alt="perfectLearning">
@@ -21,6 +22,36 @@
                     <img src="Icons/saved.svg" alt="">
                 </div>
             </div>
+            <div class="likes">
+             <p>65,952 likes</p>
+            </div>
+            <div class="caption">
+                <p>
+                    <span class="dark">myPerfectLearning</span> Coding is so much more than programming...
+                    <span class="grey">more</span> 
+                </p>
+            </div>
+            <div class="view">
+                <p class="grey">View all 1,098 comments</p>
+            </div>
+            <div class="day">
+                <p class="smallText grey">
+                    1 DAY AGO
+                </p>
+            </div>
         </div>
+       </div>
+       <div class="lower-part">
+        <div class="comment-section">
+        <i class="fa-regular fa-face-smile"></i>
+        <p class="grey comment ">Add a comment...</p>
+        </div>
+        <div class="post-comment">
+            <p class="light-blue thick">Post</p>
+        </div>
+       </div>
     </div>
+</div>
+<div class="extra">
+
 </div>

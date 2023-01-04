19165ddc4cf734350b1338dfe24d84b8ac64648f
shafiya <khanshafiya1219@gmail.com>
2023-01-04 08:52:34 +0000

    worked on story section

--- components/story/story.php
+++ components/story/story.php
@@ -1,42 +1,40 @@
 <div class="story-section">
 
+
+
+  
     <div class="story-content">
         <div class="img"><img src="image/benjamin.jfif" alt=""></div>
-        <div class="username">benjamin_button</div>
+        <div class="username">benjieee</div>
     </div>
  
-
     <div class="story-content">
 
         <div class="img"><img src="image/holly.jfif" alt=""></div>
-        <div class="username">min_holly</div>
+        <div class="username">holly</div>
     </div>
 
     <div class="story-content">
         <div class="img"><img src="image/husky.jfif" alt=""></div>
-        <div class="username">huskkyyy</div>
+        <div class="username">huskky</div>
     </div>
 
     <div class="story-content">
         <div class="img"><img src="image/meredith.jfif" alt=""></div>
-        <div class="username">meredith_grey</div>
+        <div class="username">meredith</div>
     </div>
 
     <div class="story-content">
         <div class="img"><img src="image/yeontan.jfif" alt=""></div>
-        <div class="username">yeontan_kim</div>
+        <div class="username">yeontan</div>
     </div>
     
     <div class="story-content">
         <div class="img"><img src="image/olivia.jfif" alt=""></div>
-        <div class="username">olivia.benson</div>
+        <div class="username">olivia</div>
     </div>
 
-   
+    </ul>
 
-   
-   
-
-  
    
 </div>

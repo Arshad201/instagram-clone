58ca57ee5bb337cce1b53e251c53e848598d0b26
Mushafiya <kmushafiya003@gmail.com>
2023-01-04 22:16:36 +0000

    Add more posts

--- components/post/post.php
+++ components/post/post.php
@@ -1,4 +1,8 @@
 <div class="container">
+
+<!-- ----------------------1st Post------------------------------- -->
+
+
     <div class="cards">
         <div class="upper-part">
         <div class="name">
@@ -51,7 +55,135 @@
         </div>
        </div>
     </div>
+
+    <!-- ----------------------2nd Post------------------------------- -->
+
+    <div class="cards">
+        <div class="upper-part">
+        <div class="name">
+            <div class="part1">
+              <img class ="dp-2" src="image/pw.png" alt="physicswallah">
+               <h3>PhysicsWallah</h3>
+            </div>
+            <img src="Icons/opt-dots.svg" alt="bar">
+        </div>
+        <div class="post">
+            <img class="center" src="image/post2.png" alt="post2">
+        </div>
+        <div class="content">
+            <div class="content1">
+                <div class="icons-1">
+                    <img src="Icons/noti-heart-like.svg" alt="like">
+                    <img src="Icons/comment.svg" alt="like">
+                    <img src="Icons/share.svg" alt="like">
+                </div>
+                <div class="saved">
+                    <img src="Icons/saved.svg" alt="">
+                </div>
+            </div>
+            <div class="likes">
+             <p>952 likes</p>
+            </div>
+            <div class="caption">
+                <p>
+                    <span class="dark">PhysicsWallah</span> Hide and seek 🤫...
+                    <span class="grey">more</span> 
+                </p>
+            </div>
+            <div class="view">
+                <p class="grey">View all 598 comments</p>
+            </div>
+            <div class="day">
+                <p class="smallText grey">
+                    3 DAY AGO
+                </p>
+            </div>
+        </div>
+       </div>
+       <div class="lower-part">
+        <div class="comment-section">
+        <i class="fa-regular fa-face-smile"></i>
+        <p class="grey comment ">Add a comment...</p>
+        </div>
+        <div class="post-comment">
+            <p class="light-blue thick">Post</p>
+        </div>
+       </div>
+    </div>
+
+    <!-- ----------------------3rd Post------------------------------- -->
+    <div class="cards">
+        <div class="upper-part">
+        <div class="name">
+            <div class="part1">
+              <img class ="dp-2" src="image/mered.jpg" alt="meredith_grey">
+               <h3>Cat_World_003</h3>
+            </div>
+            <img src="Icons/opt-dots.svg" alt="bar">
+        </div>
+        <div class="post">
+            <img class="center" src="image/cat.jpg" alt="post1">
+        </div>
+        <div class="content">
+            <div class="content1">
+                <div class="icons-1">
+                    <img src="Icons/noti-heart-like.svg" alt="like">
+                    <img src="Icons/comment.svg" alt="like">
+                    <img src="Icons/share.svg" alt="like">
+                </div>
+                <div class="saved">
+                    <img src="Icons/saved.svg" alt="">
+                </div>
+            </div>
+            <div class="likes">
+             <p>5,340 likes</p>
+            </div>
+            <div class="caption">
+                <p>
+                    <span class="dark">Cat_World_003</span> Cats rule the world....
+                    <span class="grey">more</span> 
+                </p>
+            </div>
+            <div class="view">
+                <p class="grey">View all 740 comments</p>
+            </div>
+            <div class="day">
+                <p class="smallText grey">
+                    5 DAY AGO
+                </p>
+            </div>
+        </div>
+       </div>
+       <div class="lower-part">
+        <div class="comment-section">
+        <i class="fa-regular fa-face-smile"></i>
+        <p class="grey comment ">Add a comment...</p>
+        </div>
+        <div class="post-comment">
+            <p class="light-blue thick">Post</p>
+        </div>
+       </div>
+    </div>
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <div class="extra">
 
 </div>
